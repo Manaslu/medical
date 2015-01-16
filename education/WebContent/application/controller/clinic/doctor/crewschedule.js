826e440d03013850c974f71dc64172163669f3aa
bruce_wang_wh <wei.wang30@pactera.com>
2015-01-16 14:50:12 +0800

    排班界面

---
+++ education/WebContent/application/controller/clinic/doctor/crewschedule.js
@@ -0,0 +1,122 @@
+define(function (require, exports, module) {
+    return function setApp(app) {
+        app.controller('ClinicDoctorCrewscheduleCtrl', ['$scope','$http' ,'$filter','DoctorsManagement','UserAppointment','$timeout',function ($scope,$http,$filter,DoctorsManagement,UserAppointment,$timeout) { 
+ 
+   
+ 
+//         var params = {
+//        		 clinicId : $scope.USER_INFO.orgCd 
+//         };
+//         DoctorsManagement.query({ //初始化用户列表
+//       		                    isArray:true,
+//       		                    params: angular.toJson(params)
+//       	                      },  function (list){
+//     		$scope.doctors =list; 
+//         });
+       	
+       	$scope.clearSchedule = function(){//重置整个日历
+       		$('#calendar').fullCalendar('destroy');
+//    		$('#calendar').fullCalendar({
+//    			header: {
+//    				left: 'prev,next',
+//    				center: 'title',
+//    				right: 'agendaWeek,month'
+//    			},
+//    			lang: 'zh-cn',
+//    			eventColor:'#333333',
+//    			 
+//    			eventLimit: true, 
+//    			buttonIcons: false,
+//    			allDaySlot : true,
+//    			slotMinutes :'60',
+//    			firstHour : '8',
+//    			slotEventOverlap:'true',
+//    			weekMode : 'variable',
+//    			editable: true,
+//    			droppable: true, // this allows things to be dropped onto the calendar
+//    			drop: function() {
+//    				alert(this.id);//div 的id
+//    				// is the "remove after drop" checkbox checked?
+//    				if ($('#drop-remove').is(':checked')) {
+//    					// if so, remove the element from the "Draggable Events" list
+//    					$(this).remove();
+//    				}
+//    			},
+//    			dayClick: function() { 
+//    				alert('a day has been clicked!'); 
+//    			},
+//    			eventClick: function(event, jsEvent, view) {
+//    				alert('an event has been clicked!'); 
+//    			}
+//    		});
+       	};
+       	$scope.saveSchedule = function(){
+       		var events =  $('#calendar').fullCalendar( 'clientEvents');
+       		alert(events);
+       	}
+ 
+        $scope.clickOnUser =  function(orderid,userid){  //点击用户名
+        	$scope.currentUserOrderId=orderid;
+        	$scope.currentUserId = userid;
+                var params = {
+                   orderId : orderid
+                };
+                DiagnoseResult.query({
+                                      isArray:true,
+                                      params: angular.toJson(params)
+                                      },
+                                      function (list){
+                $scope.currentUserResult =list; 
+                 $scope.key = list[0];
+                
+                });
+          }
+        	 
+     //------------reset botton---------------------
+            $scope.clearForm = function(){//reset botton
+                $scope.key="";
+            }
+            
+             $scope.create = function(key) { 
+             if(key.seeDoctorId){
+                   	key.seeDoctorDate=''; //update time now is number,it cause error of mismatch
+                   	DiagnoseResult.save(key,function(){
+                        $scope.clearForm();
+                        
+                        var params = {
+                       		 clinicId : $scope.USER_INFO.orgCd,
+                       		 doctorId : $scope.USER_INFO.id 
+                        };
+                      	UserAppointment.query({ //初始化用户列表
+                      		                    isArray:true,
+                      		                    params: angular.toJson(params)
+                      	                      },  function (list){
+                    		$scope.userAppointment =list; 
+                        });
+                      	     
+                      });
+               	}else{ //add
+               		key.clinicId =    $scope.USER_INFO.orgCd; 
+               		key.doctorId =    $scope.USER_INFO.id;
+               		key.mUserId =     $scope.currentUserId;
+               		key.userOrderId = $scope.currentUserOrderId;
+               		DiagnoseResult.put(key,function(){
+                       	$scope.clearForm();
+                       	var params = {
+                       		 clinicId : $scope.USER_INFO.orgCd,
+                       		 doctorId : $scope.USER_INFO.id 
+                        };
+                      	UserAppointment.query({ //初始化用户列表
+                      		                    isArray:true,
+                      		                    params: angular.toJson(params)
+                      	                      },  function (list){
+                    		$scope.userAppointment =list; 
+                        });
+                       });
+               	}
+               }
+ 
+        }]);
+    }
+
+});
